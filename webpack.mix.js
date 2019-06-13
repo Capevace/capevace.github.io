3c63975c84cb6fb6184fed4be92b7b1f6775f46d
Lukas Mateffy <capevace@gmail.com>
2019-06-13 21:34:09 +0200

    Running purgecss to make tailwind tinier

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,7 +1,19 @@
 const mix = require('laravel-mix');
-const tailwindcss = require('tailwindcss')
+const tailwindcss = require('tailwindcss');
+const purgecss = require('@fullhuman/postcss-purgecss')({
+	// Specify the paths to all of the template files in your project
+	content: ['./_site/**/*.html'],
 
-mix.js('public/_js/main.js', 'public/js/app.js').postCss('public/_css/style.css', 'public/css', [
-	require('tailwindcss'),
-	require('autoprefixer')
-]);
+	// Include any special characters you're using in this regular expression
+	defaultExtractor: content => content.match(/[A-Za-z0-9-_:/%]+/g) || []
+});
+
+mix.js('public/_js/main.js', 'public/js/app.js').postCss(
+	'public/_css/style.css',
+	'public/css',
+	[
+		require('tailwindcss'),
+		require('autoprefixer'),
+		...(process.env.NODE_ENV === 'production' ? [purgecss] : [])
+	]
+);
