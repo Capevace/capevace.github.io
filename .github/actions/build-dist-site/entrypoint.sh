5c4ac02b50f1c38032338982788808c8ec6441ab
Lukas von Mateffy <luke@mateffy.me>
2020-06-16 04:22:11 +0200

    Updated some stuff

--- .github/actions/build-dist-site/entrypoint.sh
+++ .github/actions/build-dist-site/entrypoint.sh
@@ -46,7 +46,7 @@ git add .
 git commit -m "Github Actions Deploy - $(date)"
 echo "Build branch ready to go. Pushing to Github..."
 # Force push this update to our gh-pages
-git push --force $REMOTE_REPO master:gh-pages
+git push --force $REMOTE_REPO master
 # Now everything is ready.
 # Lets just be a good citizen and clean-up after ourselves
 rm -fr .git
